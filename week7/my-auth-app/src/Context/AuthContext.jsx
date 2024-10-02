60db6244ac1d6036185e32e401dacabbb8c36d99
Viktoriia <beloborodova.viktoriia@gmail.com>
2024-10-02 10:40:38 +0300

    added morning activity #1

---
+++ week7/my-auth-app/src/Context/AuthContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const AuthContext = createContext(null); // Create a context with a default value of null
+
+export default AuthContext; // Export the AuthContext
